a1459b77b7c91fdac680526a1c554a112eb62a49
Archit Chandra <architchandra@gmail.com>
2022-03-11 19:29:58 +0530

    Remove irrelevant components and files, create App component - first pass

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
+function App() {
+  return (
+    <div>
+      <p>
+        The current count is 0
+      </p>
+    </div>
+  );
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
